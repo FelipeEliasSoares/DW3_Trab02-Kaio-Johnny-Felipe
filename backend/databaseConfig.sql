846d20d4419400c787a8d5d101184c06bf375c36
KCroce <kaiodellacroce@gmail.com>
2024-12-02 09:22:11 -0300

    PrimeiroCommitVeiculos

--- backend/databaseConfig.sql
+++ backend/databaseConfig.sql
@@ -9,28 +9,9 @@ CREATE TABLE usuario (
     softDelete BOOLEAN DEFAULT FALSE
 );
 
--- Tabela Veiculos
-CREATE TABLE Veiculos (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    placa VARCHAR(10) NOT NULL,
-    modelo VARCHAR(255) NOT NULL,
-    DataAquisicao DATE NOT NULL,
-    softDelete BOOLEAN DEFAULT FALSE
-);
-
--- Tabela MotoristasVeiculos (relacionamento entre Motoristas e Veiculos)
-CREATE TABLE MotoristasVeiculos (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    motoristaID UUID NOT NULL,
-    veiculoID UUID NOT NULL,
-    softDelete BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (motoristaID) REFERENCES Motoristas(id),
-    FOREIGN KEY (veiculoID) REFERENCES Veiculos(id)
-);
-
--- Tabela Clientes
-CREATE TABLE Clientes (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+-- Criação da tabela usuario com UUID
+CREATE TABLE usuario (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     nome VARCHAR(255) NOT NULL,
     dataCadastro DATE NOT NULL,
@@ -62,19 +43,4 @@ CREATE TABLE Login (
 -- Inserção de dados na tabela usuario com senha criptografada
 INSERT INTO usuario (email, login, senha)
 VALUES 
-    ('admin@example.com', 'admin', crypt('admin', gen_salt('bf')))  -- senha criptografada com bcrypt
-ON CONFLICT DO NOTHING;
-
--- Criação da tabela conta com UUID
-CREATE TABLE conta (
-    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-    descricao VARCHAR(255) NOT NULL,
-    tipo VARCHAR(50) NOT NULL,
-    valor NUMERIC(10, 2) NOT NULL,
-    data DATE NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    forma_pagamento VARCHAR(50) NOT NULL,
-    usuario_id UUID NOT NULL, 
-    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
-);
-
+    ('admin', crypt('admin', gen_salt('bf')), CURRENT_DATE, TRUE);
