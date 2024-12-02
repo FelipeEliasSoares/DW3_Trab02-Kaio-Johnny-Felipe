d4a7f70fc662add5b6abb705111936e474d562ce
KCroce <kaiodellacroce@gmail.com>
2024-12-02 12:05:40 -0300

    Feature-Ctl-Mdl-Veiculos

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -4,7 +4,7 @@ const routerApp = express.Router();
 const appLogin = require("../apps/login/controller/ctlLogin");
 const Me = require("../apps/middleware/auth");
 const appUsuarios = require("../apps/usuarios/controller/ctlUsuarios");
-
+const appVeiculos = require("../apps/veiculos/controller/ctlVeiculos");
 routerApp.use((req, res, next) => {
   next();
 });
@@ -18,6 +18,13 @@ routerApp.get("/", (req, res) => {
 routerApp.post("/login", appLogin.Login);
 routerApp.post("/logout", appLogin.Logout);
 routerApp.get("/api/auth/me", Me, appLogin.Me);
+
+// Rotas de Veiculos
+routerApp.get("/veiculos", Me, appVeiculos.GetAllVeiculos);
+routerApp.get("/veiculos/:id", Me, appVeiculos.GetVeiculosByID);
+routerApp.post("/veiculos", Me, appVeiculos.InsertVeiculo);
+routerApp.put("/veiculos/:id", Me, appVeiculos.UpdateVeiculo);
+routerApp.delete("/veiculos/:id", Me, appVeiculos.DeleteVeiculo);
 
 // Rotas de Usuários
 routerApp.get("/usuarios", Me, appUsuarios.GetAllUsuarios);
