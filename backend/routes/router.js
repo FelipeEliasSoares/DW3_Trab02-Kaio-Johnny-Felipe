334eaf9ea5ea79e792b22166cd8cec42f4ed101a
KCroce <kaiodellacroce@gmail.com>
2024-12-09 09:23:59 -0300

    feacture clientes

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -5,6 +5,8 @@ const appLogin = require("../apps/login/controller/ctlLogin");
 const Me = require("../apps/middleware/auth");
 const appUsuarios = require("../apps/usuarios/controller/ctlUsuarios");
 const appVeiculos = require("../apps/veiculos/controller/ctlVeiculos");
+const appClientes = require("../apps/clientes/controller/ctlClientes");
+
 routerApp.use((req, res, next) => {
   next();
 });
@@ -25,6 +27,13 @@ routerApp.get("/veiculos/:id", Me, appVeiculos.GetVeiculosByID);
 routerApp.post("/veiculos", Me, appVeiculos.InsertVeiculo);
 routerApp.put("/veiculos/:id", Me, appVeiculos.UpdateVeiculo);
 routerApp.delete("/veiculos/:id", Me, appVeiculos.DeleteVeiculo);
+
+// Rotas de Clientes
+routerApp.get("/clientes", Me, appClientes.GetAllClientes);
+routerApp.get("/clientes/:id", Me, appClientes.GetClientesByID);
+routerApp.post("/clientes", Me, appClientes.InsertCliente);
+routerApp.put("/clientes/:id", Me, appClientes.UpdateCliente);
+routerApp.delete("/clientes/:id", Me, appClientes.DeleteCliente);
 
 // Rotas de Usuários
 routerApp.get("/usuarios", Me, appUsuarios.GetAllUsuarios);
